ba7a2ffab0f6fa1b846f6f970915357bbe2bce41
NoahBalzan <111967027+NoahBalzan@users.noreply.github.com>
2022-11-15 15:20:59 +0100

    renewed

--- 3.CSS Framework/js/index.js
+++ 3.CSS Framework/js/index.js
@@ -1,4 +1,30 @@
 //Document ready
 $(function () {
-    
+/*   $("#datatable tbody tr").click(function (e) {
+    e.preventDefault();
+    var id = $(this).attr("data-id");
+    console.log(id);
+  }); */
+
+  $("#datatable .tank").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("tanke: " + id);
+  });
+
+  $("#datatable .edit").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("edit: " + id);
+  });
+
+  $("#datatable .delete").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("delete: " + id);
+  });
+  
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
 });
